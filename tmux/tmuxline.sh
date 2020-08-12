5e57be9d61d7e630673b6a0457e86ffa82f2a0e2
Dan Shick <dan.shick@gmail.com>
2020-08-12 15:42:33 -0400

    Update alacritty, tmux with gruvbox. Add swayidle. Add sqlite.

--- tmux/tmuxline.sh
+++ tmux/tmuxline.sh
@@ -1,11 +1,11 @@
 if [ "$1" = "left" ]; then
-  echo -n "#[fg=colour234, bg=colour148] $(tmux display-message -p '#S:#I.#P') >#[default]"
-  echo -n "#[fg=colour234, bg=colour33]> $(hostname | cut -d'.' -f1) >#[default]"
-  echo -n "#[fg=colour250, bg=colour24]> $( ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | tr '\n' ':' | sed 's#:.*##g' ) >#[default]>"
+  echo -n "#[fg=colour0, bg=colour11] $(tmux display-message -p '#S:#I.#P') >#[default]"
+  echo -n "#[fg=colour0, bg=colour10]> $(hostname | cut -d'.' -f1) >#[default]"
+  echo -n "#[fg=colour0, bg=colour4]> $( ip -o -4 addr show | awk -F '[ /]+' '/global/ {print $4}' | tr '\n' ':' | sed 's#:.*##g' ) >#[default]>"
 
 fi
 if [ "$1" = "right" ]; then
-  echo -n "<#[fg=colour136, bg=colour235]< $(date +"%a") <#[default]"
-  echo -n "#[fg=colour136, bg=colour235]< $(date +"%H:%M") <#[default]"
-  echo -n "#[fg=colour136, bg=colour235]< $(date +"%F") #[default]"
+  echo -n "<#[fg=colour11, bg=colour0]< $(date +"%a") <#[default]"
+  echo -n "#[fg=colour11, bg=colour0]< $(date +"%H:%M") <#[default]"
+  echo -n "#[fg=colour11, bg=colour0]< $(date +"%F") #[default]"
 fi
