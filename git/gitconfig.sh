a43a1d8e8cc6488d6261fd3961948e1a7b3e9949
Dan Shick <dan.shick@gmail.com>
2023-08-10 11:23:49 -0400

    feat(git): add aliases and global gitignore

---
+++ git/gitconfig.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+set -eux
+
+git config --global credential.helper "store"
+git config --global core.excludesfile "$HOME/.gitignore"
+git config --global init.defaultBranch "main"
+git config --global commit.gpgsign true
+git config --global core.ignorecase false
+git config --global rerere.enabled true
+git config --global push.autoSetupRemote true
+git config --global alias.rpo "remote prune origin"
+git config --global alias.bd "branch -D"
+git config --global alias.com '!git checkout "$(git symbolic-ref refs/remotes/origin/HEAD | sed s@^refs/remotes/origin/@@)"'
+git config --global alias.cob "checkout -b"
+git config --global alias.co "checkout"
+git config --global alias.prdone \
+	'!git checkout "$(git symbolic-ref refs/remotes/origin/HEAD | sed s@^refs/remotes/origin/@@)"; git pull;
+		for branch in $(git remote prune origin | grep "\\[pruned\\]" | sed "s/.*origin\\///g");
+			do git branch --format="%(refname:short)" | grep -w "${branch}" | xargs git branch -D;
+		done;'
+git config --global alias.hub \
+    '!open "$(git ls-remote --get-url | sed "s|git@github.com:\(.*\)$|https://github.com/\1|" | sed "s|\.git$||")";'
